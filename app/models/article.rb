b5e88de1a71f7d4fa493954670d2b940a8484788
josephbill <josephbill00@gmail.com>
2023-03-15 11:17:18 +0300

    starter rails access routes: resources points to create, index , show

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,21 +1,21 @@
 class Article < ApplicationRecord
-    validates :title, presence: true 
-    validates :body, presence:true, length: {minimum: 70}
+  validates :title, presence: true 
+  validates :body, presence:true, length: {minimum: 7}
 
-    # validate : indicates this is a custom validation 
-    validate :title_shouldbeunique
-    validate :body_shouldnothavecursewords 
+  validate :title_should_be_unique
+  validate :body_should_not_have_curse_words 
 
-    private 
-    def title_shouldbeunique
-      if Article.where("title = ? AND created_at >= ?", self.title, Time.zone.now.beginning_of_month).any?
-        errors.add(:title, "has already been taken this month.")
-    end 
+  private 
+  def title_should_be_unique
+    if Article.where("title = ? AND created_at >= ?", self.title, Time.zone.now.beginning_of_month).any?
+      errors.add(:title, "has already been taken this month.")
+    end
+  end 
 
-    private
-        def 
-              if self.body && self.body.match(/|damn|/i)
-                errors.add(:body, "this word is not permitted.")
-              end
-        end
+  private
+  def body_should_not_have_curse_words 
+    if self.body && self.body.match(/(damn|curseword)/i)
+      errors.add(:body, "contains a curse word.")
+    end    
+  end
 end
